b54b15f63fee766ca5f327d97a618f3cce7cd48e
Guhan Aein <102289063+GuhanAein@users.noreply.github.com>
2024-11-22 11:56:00 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,10 @@ sentences = [
     'I am going to learn DL',
     'I am going to learn NLP'
 ]
-tokenizer = Tokenizer(num_words = 100)
+#outofvocabulary token
+tokenizer = Tokenizer(num_words = 100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(sentences)
 print(word_index)
+print(sequences)
